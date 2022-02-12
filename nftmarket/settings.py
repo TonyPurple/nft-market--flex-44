4eefcabce94c7edcb788e23ad6d88646e357c61c
TonyPurple <tony.vanoni@outlook.com>
2022-02-12 14:13:19 -0800

    adjust settings

--- nftmarket/settings.py
+++ nftmarket/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure--32+t845%p#6l8l^(!58%0nrt_!s04z_qbzk19-gasis(c(=z8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
