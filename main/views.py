4a4858be1356af11b4db080de795ca511e4e3057
Marianne <m.salamanca21@live.ca>
2022-02-16 09:41:04 -0500

    fix views copy error

--- main/views.py
+++ main/views.py
@@ -39,20 +39,3 @@ class NFTCreate(CreateView):
   def form_valid(self, form):
     form.instance.user = self.request.user  
     return super().form_valid(form)
-
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      user = form.save()
-      login(request, user)
-      return redirect('home')
-    else:
-      error_message = 'Invalid sign up - try again'
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
-
-def home(request):
-  return render(request, 'home.html')
