474a0d2e11db28dd177a2291c498bac90daeb023
TonyPurple <tony.vanoni@outlook.com>
2022-02-17 10:47:08 -0800

    add nft list view for items created by any user, require login on views

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
   path('nfts/create/', views.NFTCreate.as_view(), name='nft_create'),
   path('nfts/<int:nft_id>/', views.nft_detail, name='detail'),
   path('nfts/', views.nft_index, name='nft_index'),
+  path('nfts/all/', views.NFTList.as_view(), name='nft_list'),
   path('nfts/<int:pk>/edit/', views.NFTEdit.as_view(), name='nft_edit'),
   path('nfts/<int:nft_id>/add_photo/', views.add_photo, name='add_photo'),
   path('nfts/<int:pk>/delete/', views.NFTDelete.as_view(), name='nft_delete'),
