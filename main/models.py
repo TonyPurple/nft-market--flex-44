02d543144f9c77ad616e84913f71d0fa16831714
Marianne <m.salamanca21@live.ca>
2022-02-23 21:31:28 -0500

    fix ordering for bid table

--- main/models.py
+++ main/models.py
@@ -57,9 +57,9 @@ class Bid(models.Model):
 
   def __str__(self):
     return f"{self.get_bidprice_display()} on {self.date}"
-
-class Meta:
-    ordering = ['-date']
+    
+  class Meta:
+    ordering = ('-bidprice',)
 
 class Sell(models.Model):
   sale_ends = models.DateField(("Date Sale Ends"))
